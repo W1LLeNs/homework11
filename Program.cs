543f8d59010b35fe23b7db6dbadca5f1b4badb88
W1LLeNs <vovanromanenkoff@gmail.com>
2025-01-24 12:55:52 +0200

    commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FibonacciNums
+{
+    static class Fibonacci
+    {
+        public static long GetFibonacciNumber(long num)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            if (num < 0)
+            throw new ArgumentException("Введіть ціле позитивне число!");
+            if 
+              (num == 0)
+              return 0;
+            if 
+              (num == 1) 
+              return 1;
+
+            return GetFibonacciNumber(num - 1) + GetFibonacciNumber(num - 2);
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введіть номер числа послідовності Фібоначчі:");
+            if (long.TryParse(Console.ReadLine(), out long pos))
+            {
+                try
+                {
+                    long result = Fibonacci.GetFibonacciNumber(pos - 1);
+                    Console.WriteLine($"Вхідний параметр: {pos}\n Вихідне значення: {result}");
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (StackOverflowException)
+                {
+                    Console.WriteLine("Значення занадто велике!");
+                }
+            } else
+            {
+                Console.WriteLine("Введено некоректне значення!");
+            }
+        }
+    }
+}
